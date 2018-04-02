4c199dd6ae8ec720a2d7bb54f546ff488b817c7b
sunxiuqiao <yanhejin@163.com>
2018-04-02 18:35:57 +0800

    '增加router4，增加redux，top菜单'

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,60 @@
 import React, {Component} from 'react'
 
+const style={
+    top:{
+        display:'block',
+        height:28,
+        fontSize:12,
+        backgroundColor:'#545652',
+        lineDisplay:1.62,
+        display:'block',
+        minWidth:950,
+        color:'#d5d5d5',
+        cursor:'pointer',
+    },
+    top1:{
+        display:'block',
+        margin:0,
+        padding:0
+    },
+    top_item:{
+        display:'inline-block',
+        listStyle:'none',
+        paddingTop:5
+    },
+    top_item_a:{
+        textDecorationLine:'none',
+        color:'#d5d5d5',
+        fontFamily:'Helvetica, Arial, sans-serif',
+        height:28,
+        marginRight:12, 
+    },
+    top_download:{
+        position:'relative',
+        float:'right',
+        marginRight:119,
+        fontFamily:'Helvetica, Arial, sans-serif',
+        marginTop:-27
+    },
+    top_login_div:{
+        float:'right',
+        marginRight:-214,
+        marginTop:-27
+    },
+    top_login:{
+        textDecorationLine:'none',
+        paddingLeft:12,
+        paddingRight:12,
+        display:'inline-block',
+        fontFamily:'Helvetica, Arial, sans-serif',
+        color:'#d5d5d5',
+        height:28
+    }
+
+
+    
+}
+
 class Main extends Component{
     constructor(props){
         super(props)
@@ -23,9 +78,28 @@ class Main extends Component{
 export default Main
 
 function Top(){
+    const top_items =['豆瓣', '读书','电影','音乐', '同城', '小组', '阅读', 'FM', '时间', '市集', '更多']
     return(
-        <div>
-            <div>hello world!</div>
+        <div style={style.top}>
+            <div style={style.top1}>
+                <div>
+                    <ul>
+                        {
+                            top_items.map(function(item,i){
+                                return<li key={i} style={style.top_item}><a href='' style={style.top_item_a}>{item}</a></li>
+                            })
+                        }
+                    </ul>
+                </div>
+                <div style={style.top_download}>
+                    <a href='' style={style.top_login}>下载豆瓣客户端</a>
+                </div>
+                <div style={style.top_login_div}>
+                    <a href='' style={style.top_login}>登录</a>
+                    <a href='' style={style.top_login}>注册</a>
+                </div>
+            </div>
         </div>
+        
     )
 }
